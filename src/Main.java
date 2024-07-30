8dbb1d4257083d9d6145f04693086ac5c5369b31
Saulo de Oliveira <Sauloliveira.SP@gmail.com>
2024-07-29 22:58:07 -0300

    Interligando

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,37 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.Bootcamp;
+import br.com.dio.desafio.dominio.Dev;
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("curso java");
+        curso1.setDescricao("descrição curso java");
+        curso1.setCargaHoraria(8);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("mentoria de java");
+        mentoria.setDescricao("descrição mentoria java");
+        mentoria.setData(LocalDate.now());
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev dev = new Dev();
+        dev.setNome("Saulo Oliveira");
+        dev.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos:" + dev.getConteudosInscritos());
+        dev.progredir();
+        dev.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos:" + dev.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos:" + dev.getConteudosConcluidos());
+        System.out.println("XP:" + dev.calcularTotalXp());
 
     }
 }
